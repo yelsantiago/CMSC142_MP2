f526114eb29c0c32edd0ec241382a69d3bf991dd
KaiserAce <lester.ignacio1209@gmail.com>
2024-12-24 20:43:42 +0800

    Debug

--- knapsack.py
+++ knapsack.py
@@ -8,11 +8,6 @@ def randomItemGen(n):
     for _ in range(n):
         items.append((random.randint(100, 1500), random.randint(100, 500)))
     return items
-
-class Item:
-    def __init__(self, value, weight):
-        self.value = value
-        self.weight = weight
         
 def DPKnapsack(items, WEIGHT=1000):
     V = [[0 for _ in range(WEIGHT+1)] for _ in range(len(items)+1)]
@@ -43,26 +38,29 @@ def DPMFKnapsack(items, WEIGHT=1000):
 
 
 def greedyKnapsack1(items, WEIGHT=1000):
-    arr = [Item(value, weight) for weight, value in items]
-    arr.sort(key=lambda x: x.value, reverse=True)
+    arr = items
+    arr.sort(key=lambda tup: tup[1], reverse=True)
     total_value = 0
+    included = []
     for item in arr:
-        if item.weight <= WEIGHT:
-            WEIGHT -= item.weight
-            total_value += item.value
+        if item[0] <= WEIGHT:
+            WEIGHT -= item[0]
+            total_value += item[1]
+            included.append(item)
     return total_value
 
 def greedyKnapsack2(items, WEIGHT = 1000):
-    arr = [Item(value, weight) for weight, value in items]
-    arr.sort(key=lambda x: x.weight)
+    arr = items
+    arr.sort(key=lambda tup: tup[0])
     total_value = 0
+    included = []
     for item in arr:
-        if item.weight <= WEIGHT:
-            WEIGHT -= item.weight
-            total_value += item.value
+        if item[0] <= WEIGHT:
+            WEIGHT -= item[0]
+            total_value += item[1]
+            included.append(item)
     return total_value
     
-
 def VRGreedyKnapsack(items, WEIGHT=1000):
     ratio = []
     for item in items:
@@ -80,7 +78,6 @@ def VRGreedyKnapsack(items, WEIGHT=1000):
         else:
             ratio.pop(0)
     return value
-
 
 def validityChecker():
     items1 = [(2, 12), (1, 10), (3, 20), (2, 15)]
@@ -102,4 +99,4 @@ def experimentParameters():
         i = i * 10
 
 validityChecker()
-experimentParameters()
+# experimentParameters()
